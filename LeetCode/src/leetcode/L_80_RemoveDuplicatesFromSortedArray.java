922d1542df761bdc936dd8eedde1ca5ae7e682a3
yangfan172 <yangfan172@meituan.com>
2023-06-26 16:42:39 +0800

    6.26 lc算法

--- LeetCode/src/leetcode/L_80_RemoveDuplicatesFromSortedArray.java
+++ LeetCode/src/leetcode/L_80_RemoveDuplicatesFromSortedArray.java
@@ -7,11 +7,22 @@ package leetcode;
  **/
 public class L_80_RemoveDuplicatesFromSortedArray {
     public int removeDuplicates(int[] nums) {
-        int left = 0, right = 0;
-        while (right < nums.length - 1) {
-            if (nums[right] == nums[right + 1]) {
-
-            }
+        int n = nums.length;
+        if (n <= 2) {
+            return n;
         }
+        int slow = 2, fast = 2;
+        while (fast < n) {
+            if (nums[slow - 2] != nums[fast]) {
+                nums[slow] = nums[fast];
+                ++slow;
+            }
+            ++fast;
+        }
+        return slow;
+    }
+
+    public static void main(String[] args) {
+        new L_80_RemoveDuplicatesFromSortedArray().removeDuplicates(new int[]{1, 1, 1, 2, 2, 3});
     }
 }
